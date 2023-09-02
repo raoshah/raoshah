2520ecd5e170a8bb9fc4f29147ff5d1ab55c9c16
raoshah <shahrukhrao1@gmail.com>
2023-09-02 21:52:47 +0530

    static

--- RaoShah/settings.py
+++ RaoShah/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/RaoShah/myapp/static'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
-# os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
