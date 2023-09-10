5e24c88e3961f8013e7c901ca96b5d3bdb867865
raoshah <shahrukhrao1@gmail.com>
2023-09-10 08:15:33 +0530

    added varcel

--- RaoShah/settings.py
+++ RaoShah/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--klvps563u6dgvn22_#9y$uegx5rkl@=dyi!9@yiykp1jk=@rw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['raoshah.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
